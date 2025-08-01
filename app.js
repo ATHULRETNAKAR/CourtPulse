d0a3004697401ecfe330ae00bfb303bcbf5489c0
Athul Retnakar <athulretnakar1999@gmail.com>
2025-08-01 15:30:51 +0530

    Fix: Formating Issues

--- app.js
+++ app.js
@@ -9,34 +9,32 @@ const userRouter = require("./routes/userRouter")
 const adminRouter = require("./routes/adminRouter")
 db() // dataBase connected
 
-app.set('view engine',"ejs")
+app.set('view engine', "ejs")
 app.set('views', [
   path.join(__dirname, 'views/user'),
   path.join(__dirname, 'views/admin')
 ])
 
 app.use(express.json());
-app.use('/uploads', express.static(path.join(__dirname, 'uploads'))); 
-app.use(express.static(path.join(__dirname,'public')))
-app.use(express.urlencoded({extended:true}));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }));
 app.use(session({
-    secret:process.env.SESSION_SECRET,
-    resave:false,
-    saveUninitialized:true,
-    cookie:{
-        secure:false,
-        httpOnly:true,
-        maxAge:72*60*60*1000
-    }
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    secure: false,
+    httpOnly: true,
+    maxAge: 72 * 60 * 60 * 1000
+  }
 }));
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/',userRouter);
-app.use('/admin',adminRouter);
+app.use('/', userRouter);
+app.use('/admin', adminRouter);
 
-app.listen(process.env.PORT,()=>console.log("Server Running....!"));
+app.listen(process.env.PORT, () => console.log("Server Running....!"));
 
-
-module.exports = app;
--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -5,36 +5,37 @@ const customerController = require('../controllers/admin/customerController');
 const categoryController = require('../controllers/admin/categoryController');
 const brandController = require('../controllers/admin/brandController')
 const productController = require('../controllers/admin/productController')
-const {userAuth,adminAuth,upload} = require('../middlewares/auth');
+const { userAuth, adminAuth, upload } = require('../middlewares/auth');
 
 //Login Management
-router.get('/login',adminController.loadLogin);
-router.post('/login',adminController.login);
-router.get('/dashboard',adminAuth,adminController.loadDashboard);
-router.get('/errorpage',adminController.errorpage);
-router.get('/logout',adminController.logout);
+router.get('/login', adminController.loadLogin);
+router.post('/login', adminController.login);
+router.get('/errorpage', adminController.errorpage);
+router.get('/logout', adminController.logout);
 
+router.use(adminAuth)
+router.get('/dashboard', adminController.loadDashboard);
 
 //Costomer Management
-router.get('/users',adminAuth,customerController.customerInfo);
-router.patch('/customers/:userId/toggle-status',adminAuth,customerController.toggleUserStatus);
+router.get('/users', customerController.customerInfo);
+router.patch('/customers/:userId/toggle-status', customerController.toggleUserStatus);
 
 //Category Management
-router.get('/category',adminAuth,categoryController.categoryInfo);
-router.post('/category',adminAuth, categoryController.addCategory);
-router.put('/category/:id',adminAuth, categoryController.editCategory);
-router.delete('/category/:id',adminAuth, categoryController.deleteCategory);
+router.get('/category', categoryController.categoryInfo);
+router.post('/category', categoryController.addCategory);
+router.put('/category/:id', categoryController.editCategory);
+router.delete('/category/:id', categoryController.deleteCategory);
 
 
 //Brand Management
-router.get('/brand',adminAuth,brandController.brandInfo)
-router.post('/brand',adminAuth,brandController.addBrand)
-router.put('/brand/:id',adminAuth,brandController.editBrand)
+router.get('/brand', brandController.brandInfo)
+router.post('/brand', brandController.addBrand)
+router.put('/brand/:id', brandController.editBrand)
 
 
 //Product Management
-router.get('/productInfo',adminAuth,productController.productInfo)
-router.get('/addProduct',adminAuth,productController.addProduct)
+router.get('/productInfo', productController.productInfo)
+router.get('/addProduct', productController.addProduct)
 
 
 module.exports = router;
